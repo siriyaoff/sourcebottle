226d7b160381dd04ce71ab2abdf32df16d53ce7d
siriyaoff <hwy16016@gmail.com>
2022-02-24 13:59:04 +0900

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main(int argc, char **argv){
-	string s, text="#include<bits/stdc++.h>\nusing namespace std;\ntypedef pair<int, int> pii;\ntypedef vector<int> vi;\ntypedef long long lld;\n\nint main(){\n\tios::sync_with_stdio(false);\n\tcin.tie(0);\n\t\n\treturn 0;\n}\n";
+	string s, text=""
+	"#include<iostream>\n"
+	"#include<vector>\n"
+	"#include<utility>\n"
+	"#include<algorithm>\n\n"
+	"using namespace std;\n"
+	"using pii=pair<int, int>;\n"
+	"using vi=vector<int>;\n"
+	"using lld=long long;\n\n"
+	"int main(){\n"
+	"\tios::sync_with_stdio(0);\n"
+	"\tcin.tie(0);\n\n\n"
+	"\treturn 0;\n"
+	"}";
 	s.append(argv[1]);
 	s.append(".cpp");
 	ofstream code(s.c_str());
